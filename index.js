f2674477c377b4f4c9852fe3c2cbea67ab14b8bb
Balaalfredo <balas6664@gmail.com>
2023-03-06 21:37:57 +0530

    'd'

--- index.js
+++ index.js
@@ -3,6 +3,32 @@ const border2 = document.querySelector('.border2');
 const border1 = document.querySelector('.border1');
 const border5 = document.querySelector('.border5');
 const border4 = document.querySelector('.border4');
+const border6 = document.querySelector('.border6');
+
+border6.addEventListener('mouseover', (e) => {
+    const bound = border6.getBoundingClientRect();
+    const top = bound.top;
+    const left = bound.left;
+    const ey = e.clientY;
+    const ex = e.clientX;
+    const random = Math.random() * 360;
+    const randint = Math.floor(random) + 10;
+    console.log(randint);
+
+    const newelement = document.createElement('div');
+    newelement.setAttribute('class', 'selectcircle');
+    newelement.style.left = `${ex - left}px`;
+    newelement.style.top = `${ey - top}px`;
+    newelement.style.background = `hsl(${randint},100%,50%)`;
+    // newelement.style.boxShadow = `2px 2px 20px 4px hsl(${randint},100%,50%)`;
+    border6.appendChild(newelement);
+
+    setTimeout(() => {
+        border6.removeChild(newelement);
+    }, 4000)
+})
+
+
 
 border4.addEventListener('mouseover', (e) => {
     const bound = border4.getBoundingClientRect();
